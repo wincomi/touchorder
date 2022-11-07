fb474a6de87a75b2a17069b26f8f44253d4c3ae2
Lcrescent3es <crescent3es@gmail.com>
2022-11-07 18:42:12 +0900

    로그인/리프레시/로그아웃 버그픽스1

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ require('dotenv').config({path:"./models/.env"});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
+})
 
 
 
@@ -33,47 +33,41 @@ app.get('/authent', (req, res) => {
 
 //액세스토큰 발급
 app.post('/login', async(req, res) => { //req:phone_number, res:cookie(AccessToken)
-    //if(!req.body.phone_number){
-    //    res.send('<script type="text/javascript">alert("phone_number를 입력하세요!"); document.location.href="/login";</script>')
-    //    res.end()
-    //}
-
     //db에 phone_number가 있는지 확인후 user_id 들고옴
     let phone_number = req.body.phone_number.toString();
     let user_id = await dblog.getUserId(phone_number)
-    console.log(user_id[0])
-    if (!user_id) { //후에 user_id가 null이 아닌지 확인(회원인지 확인)
+    if (user_id!==null) { //후에 user_id가 null이 아닌지 확인(회원인지 확인)
         let token=Token.generateAccessToken(user_id)
         //if(err) return res.status(400).send(err) //Bad Request
-        //쿠키에 토큰 저장
+        //클라이언트측 쿠키에 토큰 저장
         res.cookie("touch_tok", token).status(200).json({loginSuccess:true})
         //db에도 토큰 저장
         await dblog.insertToken(user_id, token)
-        res.redirect('/') //로그인하고나면 메인 페이지로 넘기기. 링크 수정 필요
+        //res.redirect('/') //로그인하고나면 메인 페이지로 넘기기. 링크 수정 필요 - 테스트할때 오류나니 주석처리함
         res.end()
-    } else {        
+    } else {
         res.send('<script type="text/javascript">alert("회원이 아닙니다."); document.location.href="/login";</script>')
-        res.end() //이거 실행되나?
+        res.end()
     }
 });
 
 // 리프레시 토큰 발급, 리프레시는 2시간정도 지속되는 토큰
-// 리프레시 토큰 작업중
-app.post("/refresh", (req, res) => { //req:cookie(AccessToken) res:cookie(RefreshToken)
+app.post("/refresh", async(req, res) => { //req:cookie(AccessToken) res:cookie(RefreshToken)
     let token = req.cookies.touch_tok
     let phone_number = req.body.phone_number
-    Token.refreshByToken(token, phone_number, (bool, Rtoken)=>{
-        if(!bool) {
-            res.send('<script type="text/javascript">alert("Rtoken failed"); document.location.href="/login";</script>')
-            res.end()
+    await Token.refreshByToken(token, async(err, Rtoken)=>{
+        if(err) {
+            res.status(403).json({refreshSuccess:false})
+            return;
         }
+        let user_id = await dblog.getUserId(phone_number)
         res.cookie("touch_tok", Rtoken).status(200).json({refreshSuccess:true})
-        dblog.insertToken(Rtoken)
-        res.redirect('/') //토큰 재발급 후 기존 페이지로 넘기기, 링크 수정 필요
+        await dblog.insertToken(user_id, Rtoken)
+        //res.redirect('/') //토큰 재발급 후 기존 페이지로 넘기기, 링크 수정 필요
         res.end()
     })
 });
-
+//나중에 쓸 코드
 app.get('/auth', auth , (req, res)=>{//auth는 미들웨어
     //미들웨어를 통과했다 = Authentication이 True라는 말
     //클라이언트에 true라는 정보 전달
@@ -87,19 +81,19 @@ app.get('/auth', auth , (req, res)=>{//auth는 미들웨어
       role: req.user.role,
       image: req.user.image
     })
-  })
-  
+})
+
 // 로그아웃
-app.get('/logout', (req, res) => {
+app.post('/logout', async(req, res) => {
     let token = req.cookies.touch_tok
-    Token.findByToken(token, (bool, user_id)=>{
-        if(!bool){
-            res.send('<script type="text/javascript">alert("logout failed"); document.location.href="/";</script>');    
-            res.end();
+    await Token.findByToken(token, async(err, user_id)=>{
+        if(err){
+            res.status(403).json({refreshSuccess:false})
+            return;
         }
-        dblog.delToken(user_id)
+        await dblog.delToken(user_id)
     })
-    res.send('<script type="text/javascript">alert("성공적으로 로그아웃 되었습니다."); document.location.href="/";</script>');    
+    res.cookie("touch_tok", "").status(200).json({logoutSuccess:true})  
     res.end();
   });
 
--- models/tokenmaker.js
+++ models/tokenmaker.js
@@ -3,12 +3,13 @@ const http = require('http');
 const jwt=require('jsonwebtoken')
 const bodyParser = require("body-parser");
 const dblog = require('../database/query/login');
+const { nextTick } = require("process");
 
 
 const Token = {}
 // access token
 Token.generateAccessToken = (word)=>{
-    return jwt.sign({ word }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "5m" });
+    return jwt.sign({ word }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "20m" });
 };
 
 // refersh token
@@ -16,65 +17,46 @@ Token.generateRefreshToken = (word)=>{
     return jwt.sign({ word }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "120m" });
 };
 
-/*
-// access token의 유효성 검사
-Token.checkAccessToken = (req, res, cbfn)=>{
-    let authHeader = req.headers["authorization"];
-    let token = authHeader && authHeader.split(" ")[1];
-    if (!token) {
-        console.log("wrong token format or token is not sended");
-        return res.sendStatus(400);
-    }
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) {
-            console.log(err);
-            return res.sendStatus(403);
-        }
 
-        req.user = user;
-        cbfn();
-    });
-};
-*/
-
-Token.findByToken = (token, cbfn)=>{
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
-        if (error) return res.sendStatus(403);
-        //decoded된 user_id로 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
-        let dbtoken=dblog.getToken(decoded)
-        console.log(dbtoken)
-        if(token!=dbtoken) { //액세스 토큰이 아니면 리프레시 토큰으로 체크 <=최적화 필요
-            jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, decoded)=>{
-                if (error) return res.sendStatus(403);
-                let dbtoken2=dblog.getToken(decoded)
-                console.log(dbtoken2)
-                if(token!=dbtoken2) return cbfn(false, null)
-                return cbfn(true, decoded)
+Token.findByToken = async(token, cbfn)=>{
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async(err, decoded)=>{
+        if (err){
+            jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, async(err, decoded)=>{
+                if (err) return cbfn(err, null);
+                decoded=decoded.word
+                let dbtoken2=await dblog.getToken(decoded)
+                if(token!=dbtoken2[0].token) return cbfn(err, null)
+                return cbfn(null, decoded)
             })
+        }else {
+            console.log("응애")
+            decoded=decoded.word
+            let dbtoken=await dblog.getToken(decoded)
+            if(token!=dbtoken[0].token) return cbfn(err, null)
+            return cbfn(null, decoded)
         }
-        return cbfn(true, decoded)
     })
 }
 
-Token.refreshByToken = (token, cbfn)=>{
+Token.refreshByToken = async(token, cbfn)=>{
     //토큰을 decode한다
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
-        if (error) return res.sendStatus(403);
-        //decoded된 user_id로 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
-        let dbtoken=dblog.getToken(decoded)
-        console.log(dbtoken)
-        if(token!=dbtoken) { //액세스 토큰이 아니면 리프레시 토큰으로 체크 <=최적화 필요
-            jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, decoded)=>{
-                if (error) return res.sendStatus(403);
-                let dbtoken2=dblog.getToken(decoded)
-                console.log(dbtoken2)
-                if(token!=dbtoken2) return cbfn(false, null)
-                dbtoken2=this.generateRefreshToken(err, decoded)
-                return cbfn(true, dbtoken2)
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async(err, decoded)=>{
+        if(err){
+            jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, async(err, decoded)=>{
+                if (err) return cbfn(err, null);
+                decoded=decoded.word
+                let dbtoken2=await dblog.getToken(decoded)
+                if(token!=dbtoken2[0].token) return cbfn(err, null)
+                dbtoken2=Token.generateRefreshToken(decoded)
+                return cbfn(null, dbtoken2)
             })
+        } else {
+            decoded=decoded.word
+            let dbtoken=await dblog.getToken(decoded)
+            if(token!=dbtoken[0].token) return cbfn(err, null)
+            dbtoken=Token.generateRefreshToken(decoded)
+            return cbfn(null, dbtoken)
         }
-        dbtoken=this.generateRefreshToken(err, decoded)
-        return cbfn(true, dbtoken)
     })
 }
 
