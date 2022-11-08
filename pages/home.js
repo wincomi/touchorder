d1902eb1fcc99941f99ff43e4a6b76f86d11938e
COMI <admin@wincomi.com>
2022-11-08 16:50:15 +0900

    client 패키지를 Next.js 형식으로 변경 (1차)

---
+++ pages/home.js
@@ -0,0 +1,17 @@
+import { Button } from 'react-bootstrap';
+import { MenuBar } from '../pages/seller/MenuBar';
+
+const Home = () => {
+    return (
+        <>
+            <MenuBar />
+            <h1>터치오더에 오신 것을 환영합니다!</h1>
+            <p>이제부터 터치 한번으로 음식을 주문해보세요.</p>
+            <div className="d-grid">
+                <Button variant="primary" href="/login" size="lg">시작하기</Button>
+            </div>
+        </>
+    );
+};
+
+export default Home;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
-export default function HomePage() {
-  return (
-    <div>
-        <main>
-        Hello World!
-        </main>
-    </div>
-  );
-}
+import Link from 'next/link'
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../styles/Choice.module.css';
+
+const Choice = () => {
+    return (
+        <>
+        <div className='container-fluid'>
+            <div className={styles.title}>터치오더에 오신 것을 환영합니다</div>
+            <div className={styles.Button}>
+                <Link href="seller/home">
+                    <Button variant="primary" size="lg">점주님</Button>
+                </Link>
+                &nbsp;&nbsp;
+                <Link href="/home">
+                    <Button variant="primary" size="lg">고객님</Button>
+                </Link>
+            </div>
+
+            <div className={styles.Footer}>
+            &nbsp;&nbsp;Presented by team Yeolilgop<br />
+            &nbsp;&nbsp;Copyright 2022 Yeolilgop, Keimyung.- All rights Reserved.
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default Choice;
