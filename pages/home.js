4766556aaf06aea996d60ac7dafb71c468f7d124
COMI <admin@wincomi.com>
2022-11-08 17:02:20 +0900

    폴더 구조 변경

--- pages/home.js
+++ pages/home.js
@@ -1,10 +1,9 @@
 import { Button } from 'react-bootstrap';
-import { MenuBar } from '../pages/seller/MenuBar';
+// import { MenuBar } from '../pages/seller/MenuBar';
 
 const Home = () => {
     return (
         <>
-            <MenuBar />
             <h1>터치오더에 오신 것을 환영합니다!</h1>
             <p>이제부터 터치 한번으로 음식을 주문해보세요.</p>
             <div className="d-grid">
