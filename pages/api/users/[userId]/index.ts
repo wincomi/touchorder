a36a66c1b32e1e1e888ac7216cb9c7851946e3d9
kmujaemin <5469730@stu.kmu.ac.kr>
2022-11-23 00:06:06 +0900

    [API] users/[phoneNumber] 에서 [userId]로 변경

--- pages/api/users/[userId]/index.ts
+++ pages/api/users/[userId]/index.ts
@@ -2,22 +2,18 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const {phoneNumber} = req.query;
+    const userId:number = Number(req.query.userId);
 
     // 타입 검사 - 안하면 14행에서 경고 표시
-    if (typeof phoneNumber !== 'string'){
-        res.status(400).json({message:"잘못된 값 입력"})
-        return;
-    }
 
     const prisma = new PrismaClient();
-    const result = await prisma.user.findFirst({ where: {phone_number:phoneNumber} });
+    const result = await prisma.user.findFirst({ where: {user_id:userId} });
     
     if (result != null) {
         res.status(200).json(result)
     } else {
         res.status(400).json({
-            "message": "해당 번호는 회원이 아닙니다."
+            "message": `해당 아이디는 회원이 아닙니다.`
         })
     }
 
