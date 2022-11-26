19904bd8c5a6eae62455fe6fc8e25058a345d246
kmujaemin <5469730@stu.kmu.ac.kr>
2022-11-27 01:25:34 +0900

    가게 Update(정보 변경) api 수정

---
+++ pages/api/stores/[store_id].ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req:NextApiRequest, res:NextApiResponse) => {
+    const store_id : number = Number(req.query.store_id);
+    const prisma = new PrismaClient();
+
+    if (isNaN(store_id)){
+        res.status(400).json({message:"store_id 는 숫자 값이어야 합니다."})
+        return;
+    }
+
+
+    // Update (수정)
+    if(req.method == "POST"){
+        let oldData:any = await prisma.store.findFirst({where:{store_id}});
+
+        let name:string     = req.body.name ?? oldData.name;
+        let address:string  = req.body.address ?? oldData.address;
+        let phone:string    = req.body.phone ?? oldData.phone;
+        let content:string  = req.body.content ?? oldData.content;
+        let deposit:number  = Number(req.body.deposit ?? oldData.deposit);
+        
+        const result = await prisma.store.update({
+            where:{store_id}, 
+            data:{
+                name,
+                address,
+                phone,
+                content,
+                deposit
+        }})
+        res.status(200).json(result);
+
+    } else {
+        res.status(400).json({message:"잘못된 요청입니다."});
+    }
+
+}
--- pages/api/stores/index.ts
+++ pages/api/stores/index.ts
@@ -52,35 +52,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                 } 
             }
             break
-        // UPDATE
-        case "PUT":
-            if (req.body.store_id == null) {
-                res.status(400).json({
-                    "message": "store_id를 입력하세요."
-                })
-            } else {
-                const createResult = await prisma.store.update({ 
-                    where: {
-                        store_id: req.body.store_id
-                    },
-                    data: {
-                        name: req.body.name != null ? req.body.name : undefined,
-                        address: req.body.address != null ? req.body.address : undefined,
-                        phone: req.body.phone != null ? req.body.phone : undefined,
-                        content: req.body.content != null ? req.body.content : undefined,
-                        deposit: !isNaN(Number(req.body.deposit)) ? Number(req.body.deposit) : undefined,
-                        primary_color: req.body.primary_color != null ? req.body.primary_color : undefined,
-                    }
-                })
-
-                if (createResult != null) {
-                    res.status(200).json(createResult)
-                } else {
-                    res.status(400).json({
-                        "message": "store를 등록할 수 없습니다."
-                    })
-                } 
-            }
-            break
+        default:
+            res.status(400).json({message:"잘못된 요청"});
     }
 }
