fd586b00c9a7656e7d568c3ca24b3f7fcbad35c3
Choi Gyu Sang <rbtkd2642@naver.com>
2022-11-27 16:14:32 +0900

    menu,review page table creating

--- pages/seller/menus.tsx
+++ pages/seller/menus.tsx
@@ -11,28 +11,42 @@ export default ({menus}) => {
    \메뉴정보
  \재고관리
  </pre>
-            <Table striped>
+ {/* 메뉴 추가 및 관리-메뉴이름, 메뉴 가격, 메뉴 설명, 메뉴 사진 */}
+            <Table>
               <thead>
                 <tr>
-                  <th>메뉴 이름</th>
+                  <th>메뉴번호</th>
+                  <th>이미지</th>
+                  <th>이름</th>
                   <th>가격</th>
                   <th>설명</th>
-                  <th>사진</th>
                 </tr>
               </thead>
+
               <tbody>
-                <tr>
-                  <th>1.컴포넌트 정의</th>
-                  <th>2.컴포넌트 상태값에 db테이블 다 넣기</th>
-                  <th>3.컴포넌트 상태값을 map함수로 테이블에 띄우기</th>
-                  <th>4.profit!!!</th>
-                </tr>
+              <tr>
+                <th>1.컴포넌트 정의</th>
+                <th>2.컴포넌트 상태값에 db테이블 다 넣기</th>
+                <th>3.컴포넌트 상태값을 map함수로 테이블에 띄우기</th>
+                <th>4.profit!!!</th>
+              </tr>
               </tbody>
             </Table>
           </SellerLayout>
     )
 }
-
+/*
+model menu {
+  menu_id   Int     @id @default(autoincrement())
+  store_id  Int
+  name      String  @db.VarChar(55)
+  content   String? @db.VarChar(255)
+  price     Int
+  category  String  @db.VarChar(55)
+  image_url String? @db.VarChar(255)
+  status    Int     @default(0)
+}
+*/ 
 
 export async function getServerSideProps() {
   const prisma = new PrismaClient()
@@ -47,3 +61,42 @@ export async function getServerSideProps() {
     props: { menus }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* <thead>
+<tr>
+<th>메뉴 이름</th>
+<th>가격</th>
+<th>설명</th>
+<th>사진</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<th>1.컴포넌트 정의</th>
+<th>2.컴포넌트 상태값에 db테이블 다 넣기</th>
+<th>3.컴포넌트 상태값을 map함수로 테이블에 띄우기</th>
+<th>4.profit!!!</th>
+</tr>
+</tbody>
+*/
