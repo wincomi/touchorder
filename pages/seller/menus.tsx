1d4f32176e169ee0e8d7ff80bc1b3cd95e190cc4
COMI <wincomi@me.com>
2022-11-28 22:16:42 +0900

    [Seller] menus.tssx 수정

--- pages/seller/menus.tsx
+++ pages/seller/menus.tsx
@@ -1,44 +1,45 @@
 import SellerLayout from "@components/seller/SellerLayout"
-import { Table } from 'react-bootstrap';
+import { Table, Button } from 'react-bootstrap';
 
-export default ({menus}) => {
+export default ({ menus }) => {
     return (
-      <div>
-        <SellerLayout>
-          <Table striped>
-            <thead>
-                <tr>
-                  <th>메뉴번호</th>
-                  <th>이미지</th>
-                  <th>이름</th>
-                  <th>가격</th>
-                  <th>카테고리</th>
-                  <th>설명</th>
-                </tr>
-            </thead>
-            <tbody id = 'menuBox'>
-              {menus.map((item) =>( 
-                <>
-                  <tr id = 'menuContent'>
-                    <th> {item.menu_id} </th>
-                    <th> {item.image_url} </th>
-                    <th> {item.name} </th>
-                    <th> {item.price} </th>
-                    <th> {item.category} </th>
-                    <th> {item.content} </th>
-                  </tr>
-                </>
-              ))}
-            </tbody>
-          </Table>
-        </SellerLayout>
-        </div>
+      <SellerLayout>
+        <h1 className="my-4">메뉴관리 <small className="h5 text-muted">메뉴정보</small></h1>
+        <Table striped>
+          <thead>
+              <tr>
+                <th>#</th>
+                <th>이미지</th>
+                <th>이름</th>
+                <th>가격</th>
+                <th>카테고리</th>
+                <th>설명</th>
+                <th>수정/삭제</th>
+              </tr>
+          </thead>
+          <tbody>
+            {menus.map((item) =>( 
+              <tr>
+                <td>{item.menu_id}</td>
+                <td>{item.image_url == null ? <span className="text-muted">이미지 없음</span> : <>TODO</>}</td>
+                <td>{item.name}</td>
+                <td>{new Intl.NumberFormat('ko-KR').format(item.price)}원</td>
+                <td>{item.category}</td>
+                <td>{item.content}</td>
+                <td>
+                  <Button variant="warning" size="sm" data-menu-id={item.menu_id}>수정</Button>{` `}
+                  <Button variant="danger" size="sm" data-menu-id={item.menu_id}>삭제</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </SellerLayout>
     )
 }
 
 export async function getStaticProps() {
-  const store_id = 1
-  //develop 브랜치랑 merge해야 정상작동할듯
+  const store_id = 1 // TODO
   const res = await fetch(`http://localhost:3000/api/stores/${store_id}/menus/`)
   const menus = await res.json()
   
@@ -46,42 +47,3 @@ export async function getStaticProps() {
     props: { menus }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* <thead>
-<tr>
-<th>메뉴 이름</th>
-<th>가격</th>
-<th>설명</th>
-<th>사진</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<th>1.컴포넌트 정의</th>
-<th>2.컴포넌트 상태값에 db테이블 다 넣기</th>
-<th>3.컴포넌트 상태값을 map함수로 테이블에 띄우기</th>
-<th>4.profit!!!</th>
-</tr>
-</tbody>
-*/
