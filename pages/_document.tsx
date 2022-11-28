8b0084f8508d839d73916cde87a59013f4bbfc3d
COMI <wincomi@me.com>
2022-11-28 15:17:03 +0900

    브라우저 타이틀 변경을 위한 _document.tsx 추가

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default () => {
+    return (
+        <Html lang="ko">
+        <Head>
+            <title>터치오더</title>
+        </Head>
+        <body>
+            <Main />
+            <NextScript />
+        </body>
+        </Html>
+    )
+}
