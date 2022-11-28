bc421e9c39b22729f8000cfb98af2ab60f3afa84
Choi Gyu Sang <rbtkd2642@naver.com>
2022-11-29 02:15:39 +0900

    order changed

--- pages/seller/orders.tsx
+++ pages/seller/orders.tsx
@@ -1,33 +1,43 @@
 import SellerLayout from "@components/seller/SellerLayout"
 import { Table } from 'react-bootstrap';
-import {PrismaClient} from "@prisma/client";
+
 
 export default ({orders}) => {
     return (
-        <div>
         <SellerLayout>
-
+          <h1 className="my-4">주문관리 <small className="h5 text-muted">주문정보 조회</small></h1>
           <Table striped>
              <thead>
                 <tr>
-                  <th>선택</th>
-                  <th>주문날짜</th>
                   <th>주문번호</th>
-                  <th>상태</th>
+                  <th>주문이 들어온 테이블</th>
+                  <th>가격</th>
+                  <th>주문날짜</th>
+
                 </tr>
               </thead>
 
               <tbody>
-                <tr>
-                  <th>접수</th>
-                  <th>2022-10-11</th>
-                  <th>10105546</th>
-                  <th>주문접수</th>
-                </tr>
+                {orders.map((item) =>(
+                  <tr>
+                    <td>{item.order_id}</td>
+                    <td>{item.table_id}</td>
+                    <td>{new Intl.NumberFormat('ko-KR').format(item.payments)}원</td>
+                    <td>{item.date}</td>
+                  </tr>
+                
+                ))}
               </tbody>
             </Table>
           </SellerLayout>
-          </div>
     )
 }
 
+export async function getStaticProps() {
+  const res = await fetch(`http://localhost:3000/api/orders`)
+  const orders = await res.json()
+
+return {
+   props: { orders }
+ }
+}
