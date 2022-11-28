1fd2450d2ed39d8f30982ca69618048103a4fa82
Lcrescent3es <crescent3es@gmail.com>
2022-11-28 19:31:59 +0900

    Merge branch 'develop-seller' of https://github.com/yeorilgop/touchorder into develop-seller

--- pages/seller/reviews.tsx
+++ pages/seller/reviews.tsx
@@ -1,11 +1,31 @@
 import SellerLayout from "../../components/SellerLayout"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-export default () => {
+
+
+export default ({Reviews}) => {
     return (
+        <div>
         <SellerLayout>
--리뷰 - reviews.tsx
- \리뷰 조회
- \리뷰 관리
-          </SellerLayout>
+    <pre>-리뷰 - reviews.tsx
+    \리뷰 조회
+    \리뷰 관리
+    </pre>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>맛있네요~!</Card.Title>
+            <Card.Text>빠누</Card.Text>
+            <Card.Text>
+              배달도 빨랐고 서비스로 음료수도 주셨습니다. 감사합니다!^^
+            </Card.Text>
+        <Button variant="primary">답변</Button>  {/*답변 버튼을 우측으로 옮겨야 함*/}
+      </Card.Body>
+    </Card>
+        
+        </SellerLayout>
+        </div>
+
     )
 }
--- pages/seller/tables.tsx
+++ pages/seller/tables.tsx
@@ -1,13 +1,30 @@
-import SellerLayout from "../../components/SellerLayout"
+import SellerLayout from "../../components/SellerLayout";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Form,Button} from 'react-bootstrap';
+import styles from '../../styles/tables.module.css';
 
-export default () => {
+
+export default (Tables) => {
     return (
+<>
+        <div>
         <SellerLayout>
--테이블 예약관리 - tables.tsx
+<pre>2
+    테이블 예약관리 - tables.tsx
  \예약조회
  \예약추가/취소
  \예약금 설정
-
-          </SellerLayout>
+</pre>
+    <Form>
+    <div className="container">
+    <div className="cell-a">1</div>
+    <div className="cell-b">2</div>
+    <div className="cell-c">3</div>
+    <div className="cell-d">4</div>
+    </div>
+    </Form>    
+        </SellerLayout>
+        </div>
+</>
     )
 }
