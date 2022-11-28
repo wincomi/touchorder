632fe954abf84e2b065a77b72df6cb2425dfb702
Lcrescent3es <crescent3es@gmail.com>
2022-11-29 00:18:41 +0900

    reviews 수정

--- pages/seller/reviews.tsx
+++ pages/seller/reviews.tsx
@@ -1,31 +1,30 @@
 import SellerLayout from "@components/seller/SellerLayout"
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Button, Card }from 'react-bootstrap';
 
-
-
-export default ({Reviews}) => {
+export default ({ reviews }) => {
     return (
-        <div>
-        <SellerLayout>
-    <pre>-리뷰 - reviews.tsx
-    \리뷰 조회
-    \리뷰 관리
-    </pre>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>맛있네요~!</Card.Title>
-            <Card.Text>빠누</Card.Text>
-            <Card.Text>
-              배달도 빨랐고 서비스로 음료수도 주셨습니다. 감사합니다!^^
-            </Card.Text>
-        <Button variant="primary">답변</Button>  {/*답변 버튼을 우측으로 옮겨야 함*/}
-      </Card.Body>
-    </Card>
-        
-        </SellerLayout>
-        </div>
-
+      <SellerLayout>
+        {reviews.map((item) => ( 
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={item.image_url1} />
+            <Card.Body>
+              <Card.Text>닉네임</Card.Text>
+              <Card.Text>
+                {item.content}
+              </Card.Text>
+              <Button variant="primary">답변</Button>  {/* 답변 버튼을 우측으로 옮겨야 함 */}
+            </Card.Body>
+          </Card>
+        ))}
+      </SellerLayout>
     )
+}
+
+export async function getStaticProps() {
+  const result = await fetch("http://localhost:3000/api/reviews/allReviewList/")
+  let reviews = await result.json()
+
+  return {
+    props: { reviews }
+  }
 }
