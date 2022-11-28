550f31d05058c4bce8a04f3f988eea7020e0ca55
COMI <wincomi@me.com>
2022-11-28 15:01:40 +0900

    [API] request.js, verify.js 파일을 typescript로 변경

---
+++ pages/api/auth/verification-code/request.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    const SMS = require('@libs/auth/sms');
+
+    if (req.method == 'POST') {
+        let phoneNumber = req.body.phoneNumber.toString()
+
+        console.log("인증 번호 요청 : " + phoneNumber)
+
+        SMS.request_verificationCode(phoneNumber)
+
+        res.status(200).json({
+            message: "요청 성공"
+        })
+    } else {
+        console.log("잘못된 양식");
+
+        return res.status(400).json({
+            message: "잘못된 양식의 요청"
+        })
+    }
+}
