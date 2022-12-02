6aba7ed963a547282d6c8b1ed0c6204dc7218ca2
Yyhis <oouoo717@gmail.com>
2022-12-02 22:38:20 +0900

    주문상태, 상세 주문, 주문 알림

--- pages/seller/order_history.tsx
+++ pages/seller/order_history.tsx
@@ -1,11 +1,59 @@
 import SellerLayout from "@components/seller/SellerLayout"
 import HeaderTitle from "@components/seller/HeaderTitle"
+import { Button, Table } from 'react-bootstrap';
 
-export default () => {
+export default ( {orders} ) => {
+  const checkedOrder = async (e) =>{
+    var orderId = e.target.getAttribute('data-order-id')
+
+    const result = await fetch(`http://localhost:3000/api/orders/update_status`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order_id: orderId, 
+        status: 2,
+      })
+    })
+   location.reload()
+  }
+
     return (
       <SellerLayout>
         <HeaderTitle title="주문" subtitle="주문 알림" />
-        <p className="text-muted">현재 기능 준비 중입니다 :)</p>
+        <Table striped>
+              <thead>
+                <tr>
+                  <th>주문번호</th>
+                  <th>주문이 들어온 테이블</th>
+                  <th>주문날짜</th>
+                  <th>주문상태</th>
+                  <th>주문정산</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {orders.map((item) =>(
+                  <tr>
+                    <td>{item.order_id}</td>
+                    <td>{item.table_id}</td>
+                    <td>{item.date}</td>
+                    <td>{item.status==1?"주문확인":""}</td>
+                    <td><Button data-order-id={item.order_id} variant="warning" size="sm" onClick={checkedOrder}>주문 완료</Button></td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
       </SellerLayout>
     )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`http://localhost:3000/api/orders/order_history`)
+  const orders = await res.json()
+
+  return {
+    props: { orders }
+  }
 }
