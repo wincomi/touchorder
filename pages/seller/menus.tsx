d811ee07e7bc4880f29410ea405c830a656a4e29
Lcrescent3es <crescent3es@gmail.com>
2022-12-04 19:44:13 +0900

    메뉴 작업중

--- pages/seller/menus.tsx
+++ pages/seller/menus.tsx
@@ -1,12 +1,25 @@
 import SellerLayout from "@components/seller/SellerLayout"
 import HeaderTitle from "@components/seller/HeaderTitle"
-import { Table, Button } from 'react-bootstrap'
+import {useState} from 'react'
+import { Table, Button, Form, Collapse } from 'react-bootstrap'
 import { InferGetStaticPropsType } from "next"
 import { menu } from "@prisma/client"
 import priceFormat from '@utils/priceFormat'
-
+/*
+  {
+    menu_id: 7,
+    store_id: 1,
+    name: '콜라',
+    content: '펩시/코카콜라',
+    price: 1600,
+    category: '음료',
+    image_url: null,
+    status: 0
+  }
+*/
 export default ({ items }: InferGetStaticPropsType<typeof getStaticProps>) => {
-    return (
+  const [isSetup,setup]=useState(false)
+  return (
       <SellerLayout>
         <HeaderTitle title="매장 관리" subtitle="상품 설정" />
         <Table striped>
@@ -14,7 +27,7 @@ export default ({ items }: InferGetStaticPropsType<typeof getStaticProps>) => {
               <tr>
                 {/*<th>#</th> */}
                 {/* <th>이미지</th> */}
-                <th>분류</th>
+                <th>#</th>
                 <th>이름</th>
                 <th>가격</th>
                 <th>설명</th>
@@ -23,23 +36,44 @@ export default ({ items }: InferGetStaticPropsType<typeof getStaticProps>) => {
           </thead>
           <tbody>
             {items.map((item) =>( 
+              <>
               <tr>
                 {/* <td>{item.menu_id}</td> */}
                 {/* <td>{item.image_url == null ? <span className="text-muted">없음</span> : <>TODO</>}</td> */}
-                <td><a href="#">{item.category}</a></td>
+                <td>{item.menu_id}</td>
                 <td>{item.name}</td>
-                <td>{priceFormat('ko-KR')}원</td>
+                <td>{priceFormat(item.price)}원</td>
                 <td>{item.content}</td>
                 <td>
-                  <Button variant="warning" size="sm" data-menu-id={item.menu_id}>수정</Button>{` `}
+                  <Button variant="warning" onClick={setup} size="sm" data-menu-id={item.menu_id}>수정</Button>{` `}
                   <Button variant="danger" size="sm" data-menu-id={item.menu_id}>삭제</Button>
                 </td>
               </tr>
+              <Form>
+                <Collapse in={isSetup}>
+                    <Form.Group>
+                        <Form.Label>인증번호</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="인증번호를 입력해주세요"
+                            value={code}
+                            onChange={inputac}
+                        />
+                        <Button variant="primary" onClick={checkCertCode}>
+                            확인
+                        </Button>
+                    </Form.Group>
+                </Collapse>
+                <div className="d-grid">
+                    <Button variant="primary" size="lg" onClick={isUser}>확인</Button>
+                </div>
+            </Form>
+              </>
             ))}
           </tbody>
         </Table>
       </SellerLayout>
-    )
+  )
 }
 
 export async function getStaticProps() {
@@ -47,7 +81,7 @@ export async function getStaticProps() {
   const res = await fetch(`http://localhost:3000/api/stores/${store_id}/menus/`)
 
   const items: menu[] = await res.json()
-  
+  console.log(items)
   return {
     props: { items }
   }
