6b9008ab8a4722e8e55f473184ab292757c53ae0
Lcrescent3es <crescent3es@gmail.com>
2022-12-04 22:00:57 +0900

    menus개발중

--- pages/seller/menus.tsx
+++ pages/seller/menus.tsx
@@ -1,25 +1,48 @@
 import SellerLayout from "@components/seller/SellerLayout"
 import HeaderTitle from "@components/seller/HeaderTitle"
-import {useState} from 'react'
+import { useState } from 'react'
 import { Table, Button, Form, Collapse } from 'react-bootstrap'
 import { InferGetStaticPropsType } from "next"
 import { menu } from "@prisma/client"
 import priceFormat from '@utils/priceFormat'
-/*
-  {
-    menu_id: 7,
-    store_id: 1,
-    name: '콜라',
-    content: '펩시/코카콜라',
-    price: 1600,
-    category: '음료',
-    image_url: null,
-    status: 0
+//이미지, state 아직 추가안함
+export default ({ items }: InferGetStaticPropsType<typeof getStaticProps>) => {              
+  const [isAdd,setAdd]=useState(false)
+  const [state, setState] = useState({name:'', content:'', price:''})
+  
+  const addMenu = async()=>{
+    const store_id=1
+    const result = await fetch(`http://localhost:3000/api/${store_id}/menus`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(state)
+    })
   }
-*/
-export default ({ items }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const [isSetup,setup]=useState(false)
-  return (
+  const updateMenu = async(menu_id:number, store_id:number)=>{
+    const update={
+      name:state.name,
+      content:state.content,
+      price:parseInt(state.price)
+    }
+    const result = await fetch(`http://localhost:3000/api/${store_id}/menus/${menu_id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(update)
+    })
+  }
+  const deleteMenu = async(menu_id:number, store_id:number)=>{
+    const result = await fetch(`http://localhost:3000/api/${store_id}/menus/${menu_id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+  }
+    return (
       <SellerLayout>
         <HeaderTitle title="매장 관리" subtitle="상품 설정" />
         <Table striped>
@@ -35,43 +58,93 @@ export default ({ items }: InferGetStaticPropsType<typeof getStaticProps>) => {
               </tr>
           </thead>
           <tbody>
-            {items.map((item) =>( 
-              <>
+            <>
+            {items.map((item) =>(
               <tr>
-                {/* <td>{item.menu_id}</td> */}
-                {/* <td>{item.image_url == null ? <span className="text-muted">없음</span> : <>TODO</>}</td> */}
-                <td>{item.menu_id}</td>
-                <td>{item.name}</td>
-                <td>{priceFormat(item.price)}원</td>
-                <td>{item.content}</td>
-                <td>
-                  <Button variant="warning" onClick={setup} size="sm" data-menu-id={item.menu_id}>수정</Button>{` `}
-                  <Button variant="danger" size="sm" data-menu-id={item.menu_id}>삭제</Button>
+              {/* <td>{item.menu_id}</td> */}
+              {/* <td>{item.image_url == null ? <span className="text-muted">없음</span> : <>TODO</>}</td> */}
+              <td>
+                {item.menu_id}
+              </td>
+              <td>                
+                <Form>
+                  <Form.Group>
+                      <Form.Control
+                          type="text"
+                          placeholder={item.name}
+                          value={state.name}
+                          onChange={(e)=>{setState({...state,name:e.target.value})}}
+                      />
+                  </Form.Group>
+                </Form>
+              </td>
+              <td>
+                <Form>
+                  <Form.Group>
+                      <Form.Control
+                          type="text"
+                          placeholder={priceFormat(item.price)}
+                          value={state.price}
+                          onChange={(e)=>{setState({...state,price:e.target.value})}}
+                      />
+                  </Form.Group>
+                </Form>
                 </td>
-              </tr>
-              <Form>
-                <Collapse in={isSetup}>
-                    <Form.Group>
-                        <Form.Label>인증번호</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="인증번호를 입력해주세요"
-                            value={code}
-                            onChange={inputac}
-                        />
-                        <Button variant="primary" onClick={checkCertCode}>
-                            확인
-                        </Button>
-                    </Form.Group>
-                </Collapse>
-                <div className="d-grid">
-                    <Button variant="primary" size="lg" onClick={isUser}>확인</Button>
-                </div>
-            </Form>
-              </>
+              <td>
+                <Form>
+                  <Form.Group>
+                      <Form.Control
+                          type="text"
+                          placeholder={item.content}
+                          value={state.content}
+                          onChange={(e)=>{setState({...state,content:e.target.value})}}
+                      />
+                  </Form.Group>
+                </Form>
+              </td>
+              <td>
+                <Button variant="warning" onClick={() => {updateMenu(item.menu_id, item.store_id)}} size="sm">수정</Button>{` `}
+                <Button variant="danger" size="sm" onClick={()=>{deleteMenu(item.menu_id, item.store_id)}}>삭제</Button>
+              </td>
+            </tr>
             ))}
+            </>
           </tbody>
         </Table>
+                <Collapse in={isAdd}>
+                  <Form>
+                    <Form.Group>
+                        <Form.Label>이름</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="메뉴 이름"
+                            value={state.name}
+                            onChange={(e)=>{setState({...state,name:e.target.value})}}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>가격</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="가격"
+                            value={state.price}
+                            onChange={(e)=>{setState({...state,price:e.target.value})}}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>설명</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="메뉴 설명"
+                            value={state.content}
+                            onChange={(e)=>{setState({...state,content:e.target.value})}}
+                        />
+                    </Form.Group>
+                  </Form>
+                </Collapse>
+                <div className="d-grid">
+                    <Button variant="primary" size="sm" onClick={()=>{if(!isAdd){setAdd(true)}else{addMenu()}}}>추가</Button>
+                </div>
       </SellerLayout>
   )
 }
@@ -81,7 +154,6 @@ export async function getStaticProps() {
   const res = await fetch(`http://localhost:3000/api/stores/${store_id}/menus/`)
 
   const items: menu[] = await res.json()
-  console.log(items)
   return {
     props: { items }
   }
