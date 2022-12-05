af504ac3af1f4caf709ba1bc2383392d719f9f9f
Choi Gyu Sang <rbtkd2642@naver.com>
2022-12-06 01:27:31 +0900

    .index페이지에 로그인 페이지 추가함.

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,105 @@ const Choice = () => {
         <>
         <div className='container-fluid'>
             <div className={styles.title}>터치오더에 오신 것을 환영합니다</div>
-
-            
         </div>
+        <Form>
+        <div className={styles.Auth_form_container}> 
+      <form className={styles.Auth_form}>
+        <div className={styles.Auth_form_content}> 
+          <h3 className={styles.Auth_form_title}>login to touchorder</h3>
+
+          <div className={styles.form_group}>
+            <input
+              type="text"
+              className="form-control mt-3"
+              placeholder="Phone Number"
+            ></input>
+            </div>
+            <Button className={styles.outline_success}>인증</Button>
+
+          <div className="form-group mt-3">
+            <input
+              type="text"
+              className="form-control mt-1"
+              placeholder="Pin Number"
+            ></input>
+          </div>
+
+          <br/>
+
+          <div className="login-button">
+          <Link href="/MyPage">  
+            <Button type="submit" className={styles.btn_success}>
+              Login
+            </Button>
+          </Link>
+          </div>
+
+          <br/>
+        </div>
+      </form>
+    </div>
+        </Form>
         </>
     );
 };
 
 export default Choice;
+
+/*
+import Link from 'next/link'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../styles/Login.module.css';
+import {Form , Button } from 'react-bootstrap';
+
+
+
+const Login = () => {
+  return (
+<>
+  <Form>
+  <div className={styles.Auth_form_container}> 
+      <form className={styles.Auth_form}>
+        <div className={styles.Auth_form_content}> 
+          <h3 className={styles.Auth_form_title}>login to touchorder</h3>
+
+          <div className={styles.form_group}>
+            <input
+              type="text"
+              className="form-control mt-3"
+              placeholder="Phone Number"
+            ></input>
+            </div>
+            <Button className={styles.outline_success}>인증</Button>
+
+          <div className="form-group mt-3">
+            <input
+              type="text"
+              className="form-control mt-1"
+              placeholder="Pin Number"
+            ></input>
+          </div>
+
+          <br/>
+
+          <div className="login-button">
+          <Link href="/MyPage">  
+            <Button type="submit" className={styles.btn_success}>
+              Login
+            </Button>
+          </Link>
+          </div>
+
+          <br/>
+        </div>
+      </form>
+    </div>
+    
+    </Form>
+
+  </>
+  );
+}
+
+export default Login;
+*/
