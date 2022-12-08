7faa2e15eb88894fa036766264c4c94ebecf43a1
Lcrescent3es <crescent3es@gmail.com>
2022-12-08 18:27:08 +0900

    코드 정리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import { SessionProvider } from "next-auth/react"
 import Router from 'next/router'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
-import { useState } from "react"
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -14,7 +13,6 @@ Router.events.on('routeChangeError', () => NProgress.done())
 NProgress.configure({ showSpinner: false })
 
 export default ({ Component, pageProps }: AppProps) => {
-    const [userId, setUserId] = useState(-1)
     
     return (
         <SessionProvider>
