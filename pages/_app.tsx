a5384006faff46f2aae37a8acd8b3a568667ffb1
COMI <wincomi@me.com>
2022-12-08 14:29:31 +0900

    app.tsx 복구

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,17 @@
-import type { AppProps } from "next/app"
+import { AppProps } from "next/app"
 import Head from "next/head"
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 import Router from 'next/router'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
-import { useState } from "react"
-import { setDefaultResultOrder } from "dns"
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done())
 NProgress.configure({ showSpinner: false })
 
-
-function MyApp({ Component, pageProps }: AppProps) {
-    const [user, setUser] = useState({})
-    //useState 선언,
-    //fetch나 axios로 디비 내용 가져옴 useState에 값 담기
-
+export default ({ Component, pageProps }: AppProps) => {
     return (
         <>
             <Head>
@@ -27,4 +21,3 @@ function MyApp({ Component, pageProps }: AppProps) {
         </>
     )
 }
-export default MyApp;
